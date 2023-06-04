c41ff80a5d672c3b5dadfaf924b42c7c192b4ef3
Kyle Barth <40353515+kyle-barth@users.noreply.github.com>
2023-06-04 21:17:49 +0100

    feat/recipe page improvements (#2)
    * refactor + added breadcrumbs to recipe pages

---
+++ src/lib/components/breadcrumbs/breadcrumbs.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let title: string;
+</script>
+
+<nav aria-label="breadcrumbs">
+	<ul class="flex gap-2 items-center justify-center font-bold">
+		<li>
+			<a href="/" class="unstyled text-white underline">Home</a>
+		</li>
+		<li>
+			<i class="fa-solid fa-angle-right" />
+		</li>
+		<li class="text-lg">{title}:</li>
+	</ul>
+</nav>
---
+++ src/lib/components/icon/icon.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	type IconSizes = '50' | '75' | '100' | '125' | '150';
+	export let scale: IconSizes = '100';
+
+	const sizes: { [key in IconSizes]: string } = {
+		50: 'scale-50 -my-5',
+		75: 'scale-75 -my-3',
+		100: 'scale-100',
+		125: 'scale-125 my-2',
+		150: 'scale-150 my-4'
+	};
+</script>
+
+<a
+	class="{sizes[
+		scale
+	]} transform unstyled hover:cursor-pointer flex flex-col items-center border-2 rounded px-2 py-1 border-dashed"
+	href="/"
+>
+	<span class="flex items-center justify-center gap-2 -mb-2">
+		<i class="fa-solid fa-utensils xs:text-2xl text-xl" />
+		<img class="w-10 h-10" src="/favicon.png" alt="Resippy" />
+		<i class="fa-solid fa-martini-glass-citrus xs:text-2xl text-xl" />
+	</span>
+
+	<span class="xs:text-4xl text-3xl font-semibold">Resippy</span>
+</a>
