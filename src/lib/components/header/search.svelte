ed014cfe529477a42c4aafa4bb6c12aa2d640de3
Kyle Barth <kylebarth@me.com>
2023-06-04 21:11:42 +0100

    refactor + added breadcrumbs to recipe pages

---
+++ src/lib/components/header/search.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { isOsMac } from '$lib/helpers/is-on-mac';
+	import { triggerSearchModal } from '$lib/helpers/trigger-search-modal';
+</script>
+
+<button
+	on:click={triggerSearchModal}
+	class="flex justify-between items-center sm:w-52 w-40 h-12 btn variant-ghost-secondary"
+>
+	<div class="flex items-center gap-2">
+		<i class="fa-solid fa-magnifying-glass" />
+		<span>Search...</span>
+	</div>
+
+	<span class="hidden sm:block">{isOsMac() ? '⌘' : 'Ctrl'}+K</span>
+</button>
