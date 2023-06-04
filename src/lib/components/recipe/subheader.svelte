dce6554a7502dc7aba4c9d47de7293ffb2ee160b
Kyle Barth <kylebarth@me.com>
2023-06-04 03:55:15 +0100

    button color lazy hotfix

--- src/lib/components/recipe/subheader.svelte
+++ src/lib/components/recipe/subheader.svelte
@@ -14,7 +14,7 @@
 		{/if}
 		<h2>{showIngredients ? 'Ingredients' : 'Steps'}</h2>
 	</span>
-	<button class="btn variant-soft-secondary" on:click={toggle}
+	<button class="btn variant-soft-secondary" style="color: white !important;" on:click={toggle}
 		>Show {showIngredients ? 'Steps' : 'Ingredients'}</button
 	>
 </div>
