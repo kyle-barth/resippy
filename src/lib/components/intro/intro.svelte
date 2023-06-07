f305c206374487603d2df9f6d52a8d0097a92985
Kyle Barth <kylebarth@me.com>
2023-06-07 21:25:25 +0100

    intro refactor

--- src/lib/components/intro/intro.svelte
+++ src/lib/components/intro/intro.svelte
@@ -5,39 +5,49 @@
 	import { fade, scale } from 'svelte/transition';
 	import Icon from '../icon/icon.svelte';
 
-	let boundingDiv: HTMLDivElement;
-
 	let animateBounce = false;
-	let animateBounceDuration = 750;
+	let animateBounceDuration = 500;
 
 	const classes = 'w-screen h-screen absolute top-0 left-0';
 
+	let initialFade = true;
+	const fadeDuration = 250;
+
+	const introExitDelay = 350;
+
 	onMount(() => {
-		animateBounce = true;
+		initialFade = false;
 
 		setTimeout(() => {
-			boundingDiv.style.opacity = '0';
+			animateBounce = true;
+
 			setTimeout(() => {
-				boundingDiv.style.display = 'none';
 				$intro = false;
-			}, animateBounceDuration);
-		}, animateBounceDuration);
+			}, animateBounceDuration + introExitDelay);
+		}, fadeDuration);
 	});
 </script>
 
 {#if $intro}
+	{#if initialFade}
+		<div class="{classes} z-[999] bg-black" out:fade={{ duration: fadeDuration }} />
+	{/if}
+
 	<div
-		bind:this={boundingDiv}
-		class="{classes} z-[999] transition-all flex justify-center items-center variant-glass-secondary"
+		out:fade={{ duration: fadeDuration }}
+		class="{classes} z-[998] transition-all flex justify-center items-center variant-glass-secondary"
 	>
 		{#if animateBounce}
-			<div transition:scale={{ duration: animateBounceDuration, opacity: 1, easing }}>
+			<div
+				transition:scale={{
+					duration: animateBounceDuration,
+					opacity: 1,
+					easing,
+					delay: fadeDuration
+				}}
+			>
 				<Icon scale="150" />
 			</div>
 		{/if}
 	</div>
-
-	{#if !animateBounce}
-		<div class="{classes} z-[998] bg-black opacity-30" out:fade={{ duration: 250 }} />
-	{/if}
 {/if}
